0937cd844a7ce1db52a85052972e8cf28f101d62
delyyyce <delyce.bentley1233@gmail.com>
2025-03-19 19:47:37 -0500

    jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import './App.css'
+import './App.css';
+import { puppyList } from './data.js';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  console.log("Puppies Array:", puppies);
+  console.log(puppyList);
+  console.log("Featured Pup:", featuredPup);
 
   return (
-  
-      <div>
-        
-      </div>
-   
-  )
+    <div className="App">
+      {puppies.map((puppy) => (
+        <p
+          onClick={() => {
+            console.log("puppy id:", puppy.id); 
+            setFeatPupId(puppy.id); 
+          }}
+          key={puppy.id}
+        >
+          {puppy.name}
+        </p>
+      ))}
+
+      {featPupId && <p>Selected Puppy ID: {featPupId}</p>}
+
+      {featuredPup && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
+
